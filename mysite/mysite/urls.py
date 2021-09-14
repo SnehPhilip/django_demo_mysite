8cd2f78bdf0cddd45552df1b465f6ac8bd58c4d6
SnehPhilip <sneh.philip@gmail.com>
2021-09-14 10:48:07 +0530

    first commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+# path to default admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
